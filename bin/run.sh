6c59b0f5c6d0dec062613a5e35ae5e66b2a6041d
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-12-19 16:30:51 +0900

    catboost_509_20201219162051_0.796

--- bin/run.sh
+++ bin/run.sh
@@ -47,7 +47,8 @@ cd ../src
 # python train_team.py -m 'catboost_505' -c 'custom_505'
 # python train_team.py -m 'catboost_506' -c 'custom_506'
 # python train_team.py -m 'catboost_507' -c 'custom_505'
-python train_team.py -m 'catboost_508' -c 'custom_507'
+# python train_team.py -m 'catboost_508' -c 'custom_507'
+python train_team.py -m 'catboost_509' -c 'custom_508'
 
 # ======================================================
 # NN
